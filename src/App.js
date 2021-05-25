71abb9e935b5cd2fcc46eefbbbea202a2e3028a3
unknown <79544694+RiverCanyon2@users.noreply.github.com>
2021-05-25 14:16:08 -0500

    We added the header, including a svg linking to home, shop linking to shop page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from '../src/components/header/header.component.jsx';
 
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
